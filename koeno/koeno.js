d98c894852fc6428859b61cbf02c30c3632fe4ea
Iky Starboy <154904513+ikyStarboy@users.noreply.github.com>
2025-10-08 16:02:54 +0700

    Update koeno.js

--- koeno/koeno.js
+++ koeno/koeno.js
@@ -1,1 +1,11 @@
-
+{
+  "title": "Koe no Katachi",
+  "banner": "https://raw.githubusercontent.com/ikyStarboy/Api-imgbb/refs/heads/main/koeno/80294717b4d93adc5d095aeed7b0f205.jpg",
+  "episodes": [
+    {
+      "id": 1,
+      "title": "1. Episode 1",
+      "video": "https://storages.sokuja.id/video2/_MOVIE_/KoeNoKatachi/sokuja.net-KNK-480p.mp4"
+    }
+  ]
+}
